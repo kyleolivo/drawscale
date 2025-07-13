f20953f53069d6769c8c0b2112beb42c86669f57
Alex Leighton <axlelonghorn@gmail.com>
2025-07-12 20:03:43 -0700

    fix(eslint): issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -8,17 +8,17 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', 'src/test/setup.js'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh', '@typescript-eslint'],
+  plugins: ['react-refresh', '@typescript-eslint', 'react'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off',
   },
   settings: {
     react: {
--- tests/unit/components/DrawCanvas.test.tsx
+++ tests/unit/components/DrawCanvas.test.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from 'react'
 import { describe, it, expect, vi, beforeEach } from 'vitest'
 import { render, screen, fireEvent } from '@testing-library/react'
 import DrawCanvas from '../../../src/components/DrawCanvas'
